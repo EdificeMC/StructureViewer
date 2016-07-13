bcf6b0465e7442c644fcb5c40d9377dd704b700b
Robert Herhold <reherhold@gmail.com>
2016-07-13 13:42:48 -0400

    Get outer stair shape working

--- src/geometries.js
+++ src/geometries.js
@@ -14,16 +14,37 @@ function stairs(blockData) {
     base.translate(0, -0.25, 0)
 
     let geometry;
-    switch(blockData.properties.shape) {
-        case 'straight':
-            let step = new THREE.BoxGeometry(1, 0.5, 0.5);
-            step.translate(0, 0.25, 0.25);
-            geometry = mergeGeometries([base, step]);
-            break;
-        default:
-            console.log('Unhandled stair block shape: ' + blockData.properties.shape);
-            geometry = new THREE.BoxGeometry(1, 1, 1);
-            break;
+    const shape = blockData.properties.shape;
+    if(shape === 'straight') {
+        let step = new THREE.BoxGeometry(1, 0.5, 0.5);
+        step.translate(0, 0.25, 0.25);
+        geometry = mergeGeometries([base, step]);
+    } else if(shape.startsWith('inner')) {
+        // The one with the mostly full block except for a quarter taken out of a corner
+        // const stepPts = [new THREE.Vector2(0, 0), new THREE.Vector2(0, 1), new THREE.Vector2(0.5, 1), new THREE.Vector2(0.5, 0.5), new THREE.Vector2(1, 0.5), new THREE.Vector2(1, 0)];
+        // const stepShape = new THREE.Shape(stepPts);
+        //
+        // // geometry = mergeGeometries([base, stepShape.extrude({amount: 0.5})]);
+        // geometry = stepShape.extrude({amount: 0.5});
+
+        let step1 = new THREE.BoxGeometry(1, 0.5, 0.5);
+        step1.translate(0, 0.25, 0.25);
+        let step2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+        step2.translate(0.25, 0.25, -0.25);
+        geometry = mergeGeometries([base, step1, step2]);
+    } else if(shape.startsWith('outer')) {
+        // The one with the mostly flat surface except the quarter block bump in the corner
+        let step = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+        if(shape.endsWith('left')) {
+            step.translate(0.25, 0.25, 0.25);
+        } else if(shape.endsWith('right')) {
+            step.translate(-0.25, 0.25, 0.25);
+        }
+        geometry = mergeGeometries([base, step]);
+    } else {
+        // Shouldn't ever happen unless a new shape is added
+        console.log('Unhandled stair block shape: ' + shape);
+        geometry = new THREE.BoxGeometry(1, 1, 1);
     }
 
     let angle;
